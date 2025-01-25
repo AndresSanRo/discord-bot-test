cca606fb0d12822e8978642a3d065c067c3ea94e
AndresSanRo <andres.san.ro@outlook.com>
2025-01-25 14:32:48 +0100

    feat: add slash commands to bot

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import discord
+from discord.ext import commands
+from discord import app_commands
 import os
 
-class Client(discord.Client):
+
+class Client(commands.Bot):
     async def on_ready(self):
-        print(f"Logged in as {self.user}")
-    
+        print(f"Logged in as {self.user}")        
+        try:
+            guild = discord.Object(id=os.environ["DISCORD_SERVER_ID"])
+            synced = await self.tree.sync(guild=guild)
+            print(f"Synced {len(synced)} commands to guild {guild.id}")
+        except Exception as e:
+            print(f"Error syncing commands: {e}")
+
     async def on_message(self, message):
         if message.author == self.user:
             return
@@ -16,6 +25,16 @@ class Client(discord.Client):
 
 intents  = discord.Intents.default()
 intents.message_content = True
+client = Client(command_prefix="!", intents=intents)
 
-client = Client(intents=intents)
+GUILD_ID = discord.Object(id=os.environ["DISCORD_SERVER_ID"])
+
+@client.tree.command(name="hello", description="Say hello", guild=GUILD_ID)
+async def hello(interaction: discord.Integration):
+    await interaction.response.send_message("Hello there!")
+
+@client.tree.command(name="print", description="I will print whatever you give me!", guild=GUILD_ID)
+async def printer(interaction: discord.Integration, printer: str):
+    await interaction.response.send_message(printer)
+
 client.run(os.environ["DISCORD_TOKEN"])
