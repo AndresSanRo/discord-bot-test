6410603b5fc9824b5086ecdcd9732c659550c112
AndresSanRo <andres.san.ro@outlook.com>
2025-01-25 15:29:49 +0100

    feat: add embed messahe

--- main.py
+++ main.py
@@ -37,4 +37,14 @@ async def hello(interaction: discord.Integration):
 async def printer(interaction: discord.Integration, printer: str):
     await interaction.response.send_message(printer)
 
+@client.tree.command(name="embed", description="Embed demo", guild=GUILD_ID)
+async def printer(interaction: discord.Integration):
+    embed = discord.Embed(title="I am a title", url="https://www.youtube.com/watch?v=xvFZjo5PgG0&ab_channel=Duran", description="I am the description", color=discord.Color.dark_purple())
+    embed.set_thumbnail(url="https://images2.minutemediacdn.com/image/upload/c_fill,w_1200,ar_4:3,f_auto,q_auto,g_auto/shape/cover/sport/649273-youtube-rick-astley-6b69666394bb6020a913c6fcd18f74be.jpg")
+    embed.add_field(name="Field 1", value="I am the field 1")
+    embed.add_field(name="Field 2", value="I am the field 2")
+    embed.add_field(name="Field 3", value="I am the field 3", inline=False)
+    embed.set_author(name=interaction.user.name, url="https://www.youtube.com/watch?v=uOpdyytB3OY&ab_channel=Protocube", icon_url="https://www.rpnation.com/data/avatars/l/36/36968.jpg?1488155304")
+    await interaction.response.send_message(embed=embed)
+
 client.run(os.environ["DISCORD_TOKEN"])
